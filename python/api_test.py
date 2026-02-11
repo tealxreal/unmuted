e2bf83f2d29d6f3cf42d954ade02311713021427
tealxreal <tealxreal@gmail.com>
2026-02-11 21:03:54 +0800

    add output

--- python/api_test.py
+++ python/api_test.py
@@ -13,6 +13,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
 FRONTEND_DIR = os.path.join(PROJECT_ROOT, "frontend")
 OUTPUT_DIR = os.path.join(PROJECT_ROOT, "output")
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 app.mount(
     "/static",
     StaticFiles(directory=FRONTEND_DIR),
