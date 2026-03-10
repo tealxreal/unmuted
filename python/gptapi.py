d7a4e7c84f15e646507c16a9a108ea4de1cf6dcc
tealxreal <tealxreal@gmail.com>
2026-03-11 02:41:46 +0800

    gpt

--- python/gptapi.py
+++ python/gptapi.py
@@ -10,12 +10,9 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 def analyze_emotion(sentence: str) -> str:
     try:
         prompt = f"""
-你是一個為藝術互動裝置服務的情緒分析系統，請依以下順序：ANGER-SURPRISE-FEAR-JOY-SADNESS-CALM進行篩選，判斷下列文字最接近哪一種情緒：
-判斷原則：依據句子中隱含的情緒做分類。
-其他規則：表達思念、想念的內容屬於SADNESS、情緒展現強度不高的句子歸類於CALM、若句子屬於直述句，依句子中詞彙的氛圍適當分配。
-句字中含期待、期望、希望、驚喜、高度正向、浪漫則歸類SURPRISE。
-
-只回傳其中一個英文單字：JOY/ANGER/SADNESS/CALM/FEAR/SURPRISE
+你是一個為藝術互動裝置服務的情緒分析系統，請依據文字中隱含的情緒做分類。
+只需要回傳其中一個英文單字：JOY/ANGER/SADNESS/CALM/FEAR/SURPRISE
+其他規則：若文字的情緒不明顯仍以CALM之外的選項優先考慮，句子真的屬於沒有形容詞的句子才歸類為CALM。
 
 文字：
 「{sentence}」
