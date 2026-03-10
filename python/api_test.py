3460fa3b5e79295bb05f751dbdff4e284682a054
tealxreal <tealxreal@gmail.com>
2026-03-10 21:38:40 +0800

    pu

--- python/api_test.py
+++ python/api_test.py
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
-FRONTEND_DIR = os.path.join(PROJECT_ROOT, "frontend")
+FRONTEND_DIR = os.path.join(PROJECT_ROOT, "public")
 OUTPUT_DIR = os.path.join(PROJECT_ROOT, "output")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 app.mount(
