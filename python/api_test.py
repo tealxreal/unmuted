22a2a8d3824dfd92f186840d895cf95bef0952c2
tealxreal <tealxreal@gmail.com>
2026-03-13 19:09:42 +0800

    p

--- python/api_test.py
+++ python/api_test.py
@@ -27,7 +27,7 @@ app.add_middleware(
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, ".."))
 FRONTEND_DIR = os.path.join(PROJECT_ROOT, "public")
-OUTPUT_DIR = os.path.join(PROJECT_ROOT, "output")
+OUTPUT_DIR = os.path.join(BASE_DIR, "output")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 app.mount(
     "/static",
